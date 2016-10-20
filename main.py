aa47bcab01dfc0177894d0746663e2b51af3b23d
Brian <b.chuang@outlook.com>
2016-10-20 16:36:03 -0400

    changed main.py to generic

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from projectUtils import Evaluator, Datifier, MusicMapper
 import math
 def main():
-	evaluator = Evaluator('x^2')
-	print(evaluator.toString())
-	datafier = Datifier(lowerBound = -10, upperBound = 10, pace = 1, evaluator = evaluator)
-	print(datafier.fillPoints())
+	expr = raw_input('Continous function: ')
+	lowBound = eval(raw_input('Lower bound: '))
+	upBound = eval(raw_input('Upper bound: '))
+	incr = eval(raw_input('Increment: '))
+	transf = raw_input('Transform function: ')
+	evaluator = Evaluator(expr)
+	datafier = Datifier(lowerBound = lowBound, upperBound = upBound, pace = incr, evaluator = evaluator)
 	mapper = MusicMapper()
 	musicMap = mapper.transformToMusic(datafier.fillPoints(), transform = Evaluator('exp(x)'))
 	print musicMap
